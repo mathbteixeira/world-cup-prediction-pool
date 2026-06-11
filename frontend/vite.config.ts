ababa6d153a9e810345c28bff0eb5cbe7a9b1fea
Matheus Batista Teixeira <mathbteixeira@gmail.com>
2026-06-11 18:23:26 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['.up.railway.app'],
+  },
   test: {
     environment: "node",
     fileParallelism: false,
